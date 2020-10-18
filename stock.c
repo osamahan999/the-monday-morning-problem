59a4c72e17866fd6129f3f660211de92a3009b58
osama <hanhan.osama@gmail.com>
2020-10-18 15:04:14 -0700

    currently getting stuck in an infinite loop not sure whats hapenning

--- stock.c
+++ stock.c
@@ -1,5 +1,7 @@
 #define _GNU_SOURCE
-#define NUM_OF_THREADS 1000
+#define NUM_OF_THREADS 10
+#define BASE_STOCK_PRICE 200
+#define PURCHASE_QUEUE_LIMIT 2
 
 #include <stdio.h>
 #include <pthread.h>
@@ -36,25 +38,32 @@ use a cv
 
 //function declarations
 void *threadFunc();
+void purchaseStock();
 
-int threadCount = 0;
+int threadCount = 0; //used to get all threads to the same program counter
+double stockPrice = BASE_STOCK_PRICE;
 
-sem_t exeOrder;
+int purchaseQueue[PURCHASE_QUEUE_LIMIT];
+int queuePointer = 0;
+
 pthread_mutex_t mutex;
+sem_t full;
+sem_t boundedBuffer;
 
 int main()
 {
-    sem_init(&exeOrder, 0, 1);
 
-    int i;
     pthread_t threadNum[NUM_OF_THREADS];
 
-    for (i = 0; i < NUM_OF_THREADS; i++)
+    sem_init(&full, 0, 1);
+    sem_init(&boundedBuffer, 0, 1);
+
+    for (int i = 0; i < NUM_OF_THREADS; i++)
     {
         pthread_create(&threadNum[i], NULL, threadFunc, NULL);
     }
 
-    for (i = 0; i < NUM_OF_THREADS; i++)
+    for (int i = 0; i < NUM_OF_THREADS; i++)
     {
         pthread_join(threadNum[i], NULL);
     }
@@ -69,6 +78,35 @@ void *threadFunc()
     pthread_mutex_unlock(&mutex);
 
     //all threads wait for count to get to 1000 before running the next code
-    while (threadCount != 1000)
+    while (threadCount != NUM_OF_THREADS)
         ;
+
+    printf("%d threadcount rn\n", threadCount);
+
+    purchaseStock();
+}
+
+void purchaseStock()
+{
+    // if full, wait
+    sem_wait(&full);
+
+    if (queuePointer == PURCHASE_QUEUE_LIMIT - 1)
+    {
+        stockPrice *= 1.01; //queue full, which means we're incrementing cost of stock
+    }
+
+    else
+    {
+        sem_post(&full); //not full
+
+        sem_wait(&boundedBuffer); //lock buffer now
+        printf("Holy shit im in!\n");
+
+        // purchaseQueue[queuePointer] = pthread_self();
+        queuePointer++;
+        sem_post(&boundedBuffer); //unlock
+    }
+
+    //if writing to buffer alrdy, wait
 }
